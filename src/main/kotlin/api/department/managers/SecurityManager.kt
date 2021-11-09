c288ff57dbc73b29b5c04359eac41c9eda0ebe55
abhirup009 <abhirup.acharya009@gmail.com>
2021-11-09 23:23:16 +0530

    Add logger and config changes

--- src/main/kotlin/api/department/managers/SecurityManager.kt
+++ src/main/kotlin/api/department/managers/SecurityManager.kt
@@ -1,16 +1,25 @@
 package api.department.managers
 
 import api.department.commons.LobbyUtils
+import api.department.controllers.DepartmentController
 import feign.FeignException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.lang.Exception
+import java.lang.SecurityManager
 
 @Service
 class SecurityManager(
     private val lobbyUtils: LobbyUtils
 ) {
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(SecurityManager::class.java)
+    }
+
     fun checkUserValidity(prospectiveUserId: String): Boolean =
         try {
+            logger.info("Checking validity for User: $$prospectiveUserId")
             lobbyUtils.getProspectiveUser(prospectiveUserId = prospectiveUserId)
                 .let {
                     true
